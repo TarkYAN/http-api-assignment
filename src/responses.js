41fe8ec08e4199d294233cd78fb076cb279d0bd1
LordOden <123093924+LordOden@users.noreply.github.com>
2024-09-22 19:21:53 -0400

    completed assignment

--- src/responses.js
+++ src/responses.js
@@ -3,9 +3,8 @@ const fs = require('fs');
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const css = fs.readFileSync(`${__dirname}/../client/style.css`);
 
-
-const respond = (request, response, content, type) => {
-    response.writeHead(200, {
+const respond = (request, response, status, content, type) => {
+    response.writeHead(status, {
         'Content-Type': type,
         'Content-Length': Buffer.byteLength(content, 'utf8'),
     });
@@ -13,16 +12,139 @@ const respond = (request, response, content, type) => {
     response.end();
 };
 
+const respondJSON = (request, response, status, object) => {
+    const jsonContent = JSON.stringify(object);
+    respond(request, response, status, jsonContent, 'application/json');
+};
+
+const respondXML = (request, response, status, xmlString) => {
+    respond(request, response, status, xmlString, 'text/xml');
+};
+
+const respondFile = (request, response, status, fileContent, contentType) => {
+    respond(request, response, status, fileContent, contentType);
+};
+
 const getIndex = (request, response) => {
-    respond(request, response, index, 'text/html');
+    respondFile(request, response, 200, index, 'text/html');
 };
 
 const getCSS = (request, response) => {
-    respond(request, response, css, 'text/css');
-  };
+    respond(request, response, 200, css, 'text/css');
+};
 
+const success = (request, response) => {
+    const responseJSON = {
+        message: 'This is a successful response',
+    };
+
+    if (request.acceptedTypes.includes('text/xml')) 
+    {
+        const xmlResponse = `<response><message>${responseJSON.message}</message></response>`;
+        respondXML(request, response, 200, xmlResponse);
+    } 
+    else 
+    {
+        respondJSON(request, response, 200, responseJSON);
+    }
+};
+
+const badRequest = (request, response, params) => {
+    const responseJSON = { message: 'Missing valid query parameter set to true', id: 'badRequest' };
+
+    if (params.valid === 'true') 
+    {
+        const successJSON = { message: 'This is a successful request' };
+        if (request.acceptedTypes.includes('text/xml')) 
+        {
+            const xmlResponse = `<response><message>${successJSON.message}</message></response>`;
+            return respondXML(request, response, 200, xmlResponse);
+        }
+        return respondJSON(request, response, 200, successJSON);
+    }
+
+    if (request.acceptedTypes.includes('text/xml')) 
+    {
+        const xmlResponse = `<response><message>${responseJSON.message}</message><id>${responseJSON.id}</id></response>`;
+        return respondXML(request, response, 400, xmlResponse);
+    }
+    return respondJSON(request, response, 400, responseJSON);
+};
+
+const unauthorized = (request, response, params) => {
+    const responseJSON = { message: 'Missing loggedIn query parameter set to yes', id: 'unauthorized' };
+
+    if (params.loggedIn === 'yes') 
+    {
+        const successJSON = { message: 'You have successfully logged in' };
+        if (request.acceptedTypes.includes('text/xml')) 
+        {
+            const xmlResponse = `<response><message>${successJSON.message}</message></response>`;
+            return respondXML(request, response, 200, xmlResponse);
+        }
+        return respondJSON(request, response, 200, successJSON);
+    }
+
+    if (request.acceptedTypes.includes('text/xml')) 
+    {
+        const xmlResponse = `<response><message>${responseJSON.message}</message><id>${responseJSON.id}</id></response>`;
+        return respondXML(request, response, 401, xmlResponse);
+    }
+    return respondJSON(request, response, 401, responseJSON);
+};
+
+const forbidden = (request, response) => {
+    const responseJSON = { message: 'You do not have access to this content', id: 'forbidden' };
+
+    if (request.acceptedTypes.includes('text/xml')) 
+    {
+        const xmlResponse = `<response><message>${responseJSON.message}</message><id>${responseJSON.id}</id></response>`;
+        return respondXML(request, response, 403, xmlResponse);
+    }
+    return respondJSON(request, response, 403, responseJSON);
+};
+
+const internal = (request, response) => {
+    const responseJSON = { message: 'Internal Server Error. Something went wrong.', id: 'internalError' };
+
+    if (request.acceptedTypes.includes('text/xml')) 
+    {
+        const xmlResponse = `<response><message>${responseJSON.message}</message><id>${responseJSON.id}</id></response>`;
+        return respondXML(request, response, 500, xmlResponse);
+    }
+    return respondJSON(request, response, 500, responseJSON);
+};
+
+const notImplemented = (request, response) => {
+    const responseJSON = { message: 'A GET request for this page has not been implemented yet. Check again later for updated content.', id: 'notImplemented' };
+
+    if (request.acceptedTypes.includes('text/xml')) 
+    {
+        const xmlResponse = `<response><message>${responseJSON.message}</message><id>${responseJSON.id}</id></response>`;
+        return respondXML(request, response, 501, xmlResponse);
+    }
+    return respondJSON(request, response, 501, responseJSON);
+};
+
+const notFound = (request, response) => {
+    const responseJSON = { message: 'The page you are looking for was not found.', id: 'notFound' };
+
+    if (request.acceptedTypes.includes('text/xml')) 
+    {
+        const xmlResponse = `<response><message>${responseJSON.message}</message><id>${responseJSON.id}</id></response>`;
+        return respondXML(request, response, 404, xmlResponse);
+    }
+    return respondJSON(request, response, 404, responseJSON);
+};
 
 module.exports = {
     getIndex,
-    getCSS
+    getCSS,
+    success,
+    badRequest,
+    unauthorized,
+    forbidden,
+    internal,
+    notImplemented,
+    notFound,
 };
--- src/server.js
+++ src/server.js
@@ -1,32 +1,55 @@
 const http = require('http');
+const url = require('url');
 const responseHandler = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const urlStruct = {
-    '/': responseHandler.getIndex,
-    '/style.css': responseHandler.getCSS,
-    '/success': responseHandler.success,
-    index: responseHandler.getIndex
-};
-
 const onRequest = (request, response) => {
-
     console.log(request.url);
 
-    const protocol = request.connection.encrypted ? 'https' : 'http';
-    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
-    request.acceptedTypes = request.headers.accept.split(',');
+    const parsedUrl = url.parse(request.url, true);
+    request.acceptedTypes = request.headers.accept ? request.headers.accept.split(',') : ['application/json'];
 
-    if (urlStruct[parsedUrl.pathname]) {
-        urlStruct[parsedUrl.pathname](request, response);
+    const params = parsedUrl.query;
+
+
+    if (parsedUrl.pathname === '/') 
+    {
+        responseHandler.getIndex(request, response);
     }
-    else {
-        urlStruct.index(request, response);
+    else if (parsedUrl.pathname === '/style.css') 
+    {
+        responseHandler.getCSS(request, response);
+    }
+    else if (parsedUrl.pathname === '/success') 
+    {
+        responseHandler.success(request, response);
+    }
+    else if (parsedUrl.pathname === '/badRequest') 
+    {
+        responseHandler.badRequest(request, response, params);
+    }
+    else if (parsedUrl.pathname === '/unauthorized') 
+    {
+        responseHandler.unauthorized(request, response, params);
+    }
+    else if (parsedUrl.pathname === '/forbidden') 
+    {
+        responseHandler.forbidden(request, response);
+    }
+    else if (parsedUrl.pathname === '/internal') 
+    {
+        responseHandler.internal(request, response);
+    }
+    else if (parsedUrl.pathname === '/notImplemented') 
+    {
+        responseHandler.notImplemented(request, response);
+    }
+    else 
+    {
+        responseHandler.notFound(request, response);
     }
 };
-    
-
 
 http.createServer(onRequest).listen(port, () => {
     console.log(`Listening on 127.0.0.1:${port}`);
